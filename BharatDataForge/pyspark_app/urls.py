c5c07fb50628d8f4252d1e29dce54551e639676a
switchity <switchitynetworks@gmail.com>
2026-06-23 16:58:42 +0530

    adding new code flow

--- BharatDataForge/pyspark_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("home/", views.file_selection_page, name="file_selection_page"),
-]
---
+++ application_pysparks/BharatDataForge/pyspark_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("home/", views.file_selection_page, name="file_selection_page"),
+    path("home/", views.file_selection_page, name="file_selection_page"),
+]
